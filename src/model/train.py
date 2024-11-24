afc9a31bab08f44cf7e504cd6c7132f0c208abdd
Tomás Hansen <tomas.hansen@ug.uchile.cl>
2024-11-24 12:41:31 -0300

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -55,7 +55,7 @@ def parse_args():
                         type=str)
     parser.add_argument("--reg_rate", dest='reg_rate',
                         type=float, default=0.001)
-
+    #Justo pullrequest
     # parse args
     args = parser.parse_args()
 
