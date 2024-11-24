0778427c171aeb4510ad9923c3aaba9261f75349
Tomás Hansen <tomas.hansen@ug.uchile.cl>
2024-11-24 11:40:17 -0300

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,5 +1,4 @@
 # Import libraries
-
 import argparse
 import glob
 import os
@@ -9,18 +8,19 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-# define functions
+
+# Define functions
 def main(args):
-    # TO DO: enable autologging
+    # Enable autologging
     mlflow.autolog()
 
-    # read data
+    # Read data
     df = get_csvs_df(args.training_data)
 
-    # split data
+    # Split data
     X_train, X_test, y_train, y_test = split_data(df)
 
-    # train model
+    # Train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
@@ -33,47 +33,45 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
 def split_data(df):
     y = df["Diabetic"]
     X = df.drop("Diabetic", axis=1)
     X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=123)
+        X, y, test_size=0.2, random_state=123
+    )
     return X_train, X_test, y_train, y_test
 
+
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    # Train model
+    model = LogisticRegression(C=1 / reg_rate, solver="liblinear")
+    model.fit(X_train, y_train)
 
 
 def parse_args():
-    # setup arg parser
+    # Setup arg parser
     parser = argparse.ArgumentParser()
 
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    # Add arguments
+    parser.add_argument("--training_data", dest='training_data', type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
 
-    # parse args
+    # Parse args
     args = parser.parse_args()
 
-    # return args
     return args
 
-# run script
+
+# Run script
 if __name__ == "__main__":
-    # add space in logs
     print("\n\n")
     print("*" * 60)
 
-    # parse args
+    # Parse args
     args = parse_args()
 
-    # run main function
+    # Run main function
     main(args)
 
-    # add space in logs
     print("*" * 60)
     print("\n\n")
